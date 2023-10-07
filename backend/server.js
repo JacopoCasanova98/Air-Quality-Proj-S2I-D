0a817dadf3193f474ea7874378ce66fe41c76ae5
JacopoCasanova98 <131349460+JacopoCasanova98@users.noreply.github.com>
2023-10-07 15:32:15 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,14 @@ dotenv.config();
 const app = express();
 
 // Configurazione CORS
-app.use(cors({
-  origin: 'https://cute-puppy-86d72c.netlify.app/',
-}));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://cute-puppy-86d72c.netlify.app');
+  // Specifica i metodi consentiti, le intestazioni personalizzate e altre configurazioni CORS se necessario.
+  // Esempio:
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 // Middlewares
 app.use(express.json());
